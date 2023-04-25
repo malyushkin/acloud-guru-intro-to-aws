ec9f7ee520cf87b198b7177b2811a4967aa14987
Roman Maliushkin <malyushkinr@gmail.com>
2023-04-25 16:54:19 +0200

    Add course structure

---
+++ utils/utils.py
@@ -0,0 +1,33 @@
+import yaml
+
+"""
+Templates
+* _H - headers
+"""
+TITLE_H = "# {title}\n"
+SECTION_H = "## {title}\n"
+SUBSECTION_H = "1. {title}\n\n"
+
+
+def write_info(yml, name="README", ext="md"):
+    with open(f"../{name}.{ext}", "w") as inf:
+        # Write title
+        inf.write(TITLE_H.format(title=yml["title"]))
+
+        for section in yml["content"]:
+            # Write section
+            inf.write(SECTION_H.format(title=section["title"]))
+
+            for subsection in section["content"]:
+                # Write subsection
+                inf.write(SUBSECTION_H.format(title=subsection["title"]))
+
+
+if __name__ == "__main__":
+    with open("course.yml", "r") as yml:
+        try:
+            data = yaml.safe_load(yml)
+        except yaml.YAMLError as exception:
+            raise exception
+
+    write_info(data)
